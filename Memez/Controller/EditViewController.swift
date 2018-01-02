12a209fc94858102738e9e8aeeabae1ead694882
hassaneldesouky <hassaneldesouky@icloud.com>
2018-01-02 09:30:37 +0200

    Complete version 1

--- Memez/Controller/EditViewController.swift
+++ Memez/Controller/EditViewController.swift
@@ -13,6 +13,13 @@ class EditViewController: UIViewController, UIImagePickerControllerDelegate, UIN
 
     let imagePicker = UIImagePickerController()
    
+    
+    // array to save the images
+    var sentImges: [Meme] {
+        return savedImage
+    }
+    
+    
     //MARK: OUTLETS
     
     @IBOutlet weak var navBar: UINavigationBar!
@@ -39,6 +46,9 @@ class EditViewController: UIViewController, UIImagePickerControllerDelegate, UIN
         topText.text = "TOP"
         bottomText.text = "BOTTOM"
         
+        // disable share button
+        shareButton.isEnabled = false
+        
         // function to apply styles
         func textStyles(text: UITextField) {
             // style the field
@@ -50,7 +60,7 @@ class EditViewController: UIViewController, UIImagePickerControllerDelegate, UIN
                 NSAttributedStringKey.strokeWidth.rawValue: -3.0,
                 NSAttributedStringKey.foregroundColor.rawValue: UIColor.white,
                 NSAttributedStringKey.strokeColor.rawValue: UIColor.black,
-                NSAttributedStringKey.font.rawValue: UIFont(name: "Impact", size: 30)!,
+                NSAttributedStringKey.font.rawValue: UIFont(name: "Impact", size: 45)!,
             ]
             
             topText.defaultTextAttributes = textAttributes
@@ -65,8 +75,74 @@ class EditViewController: UIViewController, UIImagePickerControllerDelegate, UIN
     }
     
     
-
+    
+    //MARK: KEYBOARD
+    
+    
+    // get keyboard hight
+    func keyboardHight(notification: Notification) -> CGFloat {
+        let userInfo = notification.userInfo
+        let keyboardSize = userInfo![UIKeyboardFrameEndUserInfoKey] as! NSValue
+        //return
+        return keyboardSize.cgRectValue.height
+    }
+    
+    // control sliding up and down of view
+    @objc func keyboardWillShow(_ notification: Notification) {
+        if bottomText.isFirstResponder {
+            view.frame.origin.y = 0 - keyboardHight(notification: notification)
+        }
+    }
+    
+    @objc func keyboardWillHide(_ notification: Notification) {
+        if bottomText.isFirstResponder {
+            view.frame.origin.y = 0
+        }
+    }
+    
+    
+    // subscribe to keyboard notifications
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(true)
+        subscribeToKeyboardNotifications()
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(true)
+        unsubscribeFromKeyboardNotifications()
+    }
+    
+    func subscribeToKeyboardNotifications() {
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(_:)), name: .UIKeyboardWillShow, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(_:)), name: .UIKeyboardWillHide, object: nil)
+    }
+    
+    func unsubscribeFromKeyboardNotifications() {
+        NotificationCenter.default.removeObserver(self, name: .UIKeyboardWillShow, object: nil)
+        NotificationCenter.default.removeObserver(self, name: .UIKeyboardWillHide, object: nil)
+    }
+    
+    // return key dismisses keyboard
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+       return view.endEditing(true)
+    }
+    
+    // click away dismisses keyboard
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    
+    // replace the default text with the user's
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        if textField.text == "TOP" || textField.text == "BOTTOM" {
+            textField.text = ""
+        }
+    }
+    
+    
    
+    
+    
     //MARK: IMAGE SELECTION
     
      // camera button
@@ -74,7 +150,7 @@ class EditViewController: UIViewController, UIImagePickerControllerDelegate, UIN
         // check if a camera is available on the device, disable button if not
         imagePicker.sourceType = .camera
         present(imagePicker, animated: true, completion: nil)
-
+        shareButton.isEnabled = true
     }
     
     // select an image from album
@@ -88,6 +164,7 @@ class EditViewController: UIViewController, UIImagePickerControllerDelegate, UIN
         if let selectedImage = info[UIImagePickerControllerOriginalImage] as? UIImage! {
             photoView.contentMode = .scaleAspectFill
             photoView.image = selectedImage
+            shareButton.isEnabled = true
         }
             dismiss(animated: true, completion: nil)
     }
@@ -105,6 +182,50 @@ class EditViewController: UIViewController, UIImagePickerControllerDelegate, UIN
         dismiss(animated: true, completion: nil)
     }
     
+    
+    //MARK: CREATE IMAGE
+    
+    
+    func generateMemedImage() -> UIImage {
+        // hide navbar and toolbar
+        navBar.isHidden = true
+        toolBar.isHidden = true
+        // render image
+        UIGraphicsBeginImageContext(self.view.frame.size)
+        view.drawHierarchy(in: self.view.frame, afterScreenUpdates: true)
+        let finalImage: UIImage = UIGraphicsGetImageFromCurrentImageContext()!
+        UIGraphicsEndImageContext()
+        // show toolbar and navbar
+        navBar.isHidden = false
+        toolBar.isHidden = false
+        
+        return finalImage
+    }
+    
+    
+    //MARK: SAVE IMAGE
+    
+    
+    func saveImage() {
+        let image = Meme(topText: topText.text!, bottomText: bottomText.text!, image: photoView.image!, finalImage: generateMemedImage())
+        savedImage.append(image)
+    }
+    
+    
+    
+    
+    //MARK: SHARE IMAGE
+    
+    
+    @IBAction func shareAction(_ sender: UIBarButtonItem) {
+        let shareImage = generateMemedImage()
+        let activityController = UIActivityViewController(activityItems: [shareImage], applicationActivities: nil)
+        activityController.completionWithItemsHandler = {
+            (activity, success, itemes, error) in
+            self.saveImage()
+        }
+        present(activityController, animated: true, completion: nil)
+    }
     
 }
 
---
+++ Memez/Model/Meme.swift
@@ -0,0 +1,22 @@
+//
+//  Meme.swift
+//  Memez
+//
+//  Created by Hassan El Desouky on 1/2/18.
+//  Copyright © 2018 Hassan El Desouky. All rights reserved.
+//
+
+import UIKit
+import Foundation
+
+
+// creating a struct to store the image "meme" properties
+struct Meme {
+    var topText: String
+    var bottomText: String
+    var image: UIImage
+    var finalImage: UIImage
+}
+
+var savedImage = [Meme]()
+
